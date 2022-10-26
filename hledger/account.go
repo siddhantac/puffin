1f4f2c44fe18e2b1a7ba310e7d103bab76e0ba98
Siddhanta <siddhanta.c@foodpanda.com>
2022-10-26 10:18:34 +0800

    Add account.go

---
+++ hledger/account.go
@@ -0,0 +1,42 @@
+package hledger
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+)
+
+type Account struct {
+	Name   string
+	Amount string
+}
+
+func parseAccountsFromCSV(data io.Reader) []Account {
+	accs := make([]Account, 0)
+
+	csvReader := csv.NewReader(data)
+
+	_, _ = csvReader.Read() // ignore the first row
+
+	for {
+		record, err := csvReader.Read()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			fmt.Println("error:", err)
+			break
+		}
+
+		acc := Account{
+			Name:   record[0],
+			Amount: record[1],
+		}
+
+		accs = append(accs, acc)
+	}
+
+	return accs
+}
