f801de280f9dd2f502d0946f10ff931562b8aaec
Siddhanta <siddhanta.c@gmail.com>
2023-04-25 18:22:03 +0800

    Add date filter

---
+++ ledger/filter.go
@@ -0,0 +1,45 @@
+package ledger
+
+import (
+	"fmt"
+	"time"
+)
+
+func (t Transactions) FilterByDate(before, after time.Time) (Transactions, error) {
+	if before.IsZero() && after.IsZero() {
+		return t, nil
+	}
+
+	filteredTransactions := make(Transactions, 0)
+	if !before.IsZero() && !after.IsZero() {
+		for _, txn := range t {
+			if txn.Date.Before(before) && txn.Date.After(after) {
+				filteredTransactions = append(filteredTransactions, txn)
+			}
+		}
+
+		return filteredTransactions, nil
+	}
+
+	if !before.IsZero() {
+		for _, txn := range t {
+			if txn.Date.Before(before) {
+				filteredTransactions = append(filteredTransactions, txn)
+			}
+		}
+
+		return filteredTransactions, nil
+	}
+
+	if !after.IsZero() {
+		for _, txn := range t {
+			if txn.Date.After(after) {
+				filteredTransactions = append(filteredTransactions, txn)
+			}
+		}
+
+		return filteredTransactions, nil
+	}
+
+	return nil, fmt.Errorf("something wrong with the dates")
+}
---
+++ ledger/filter_test.go
@@ -0,0 +1,97 @@
+package ledger
+
+import (
+	"testing"
+	"time"
+
+	"github.com/howeyc/ledger/decimal"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilterByDate(t *testing.T) {
+	txns := Transactions{
+		{
+			Date:  time.Now(),
+			Payee: "test payee",
+			AccountChanges: []Account{
+				{
+					Name:    "expenses:rent",
+					Balance: decimal.NewFromFloat(150.23),
+				},
+				{
+					Name:    "income:salary",
+					Balance: decimal.NewFromFloat(-150.23),
+				},
+			},
+		},
+		{
+			Date:  time.Now().Add(-48 * time.Hour),
+			Payee: "test payee 2",
+			AccountChanges: []Account{
+				{
+					Name:    "expenses:food",
+					Balance: decimal.NewFromFloat(20.50),
+				},
+				{
+					Name:    "income:interest",
+					Balance: decimal.NewFromFloat(-20.50),
+				},
+			},
+		},
+	}
+
+	tests := map[string]struct {
+		before             time.Time
+		after              time.Time
+		assertError        func(*testing.T, error)
+		assertTransactions func(*testing.T, Transactions)
+	}{
+		"all transactions are between before and after": {
+			after:       time.Now().Add(-72 * time.Hour),
+			before:      time.Now().Add(24 * time.Hour),
+			assertError: func(t *testing.T, err error) { assert.NoError(t, err) },
+			assertTransactions: func(t *testing.T, gotTxns Transactions) {
+				assert.Equal(t, txns, gotTxns)
+			},
+		},
+		"only 1 transaction is within before and after": {
+			after:       time.Now().Add(-24 * time.Hour),
+			before:      time.Now().Add(1 * time.Hour),
+			assertError: func(t *testing.T, err error) { assert.NoError(t, err) },
+			assertTransactions: func(t *testing.T, gotTxns Transactions) {
+				assert.Len(t, gotTxns, 1)
+				assert.Equal(t, txns[0], gotTxns[0])
+			},
+		},
+		"only after is provided": {
+			after:       time.Now().Add(-24 * time.Hour),
+			assertError: func(t *testing.T, err error) { assert.NoError(t, err) },
+			assertTransactions: func(t *testing.T, gotTxns Transactions) {
+				assert.Len(t, gotTxns, 1)
+				assert.Equal(t, txns[0], gotTxns[0])
+			},
+		},
+		"only before is provided": {
+			before:      time.Now().Add(-24 * time.Hour),
+			assertError: func(t *testing.T, err error) { assert.NoError(t, err) },
+			assertTransactions: func(t *testing.T, gotTxns Transactions) {
+				assert.Len(t, gotTxns, 1)
+				assert.Equal(t, txns[1], gotTxns[0])
+			},
+		},
+		"when no timestamps are provided it returns all transactions": {
+			assertError: func(t *testing.T, err error) { assert.NoError(t, err) },
+			assertTransactions: func(t *testing.T, gotTxns Transactions) {
+				assert.Equal(t, txns, gotTxns)
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			gotTxns, err := txns.FilterByDate(test.before, test.after)
+			test.assertError(t, err)
+			test.assertTransactions(t, gotTxns)
+		})
+	}
+}
