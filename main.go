7294ee7c7fa6dae319bdbedc9d45eb12d9cb7b04
Siddhanta <siddhanta.c@gmail.com>
2023-04-25 18:29:13 +0800

    Add some test code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"puffin/ledger"
 )
@@ -16,13 +17,26 @@ func main() {
 		return
 	}
 
-	txns, err := ledger.ParseLedgerFile(os.Args[1])
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	txns, err := ledger.ParseLedger(f)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	for i, t := range txns {
+	filtered, err := txns.FilterByDate(time.Date(2022, 1, 1, 0, 0, 0, 0, &time.Location{}), time.Time{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for i, t := range filtered {
 		fmt.Println(t)
 		if i > 10 {
 			return
