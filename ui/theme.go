d5bea429bc554e73c223b499c752a2f2e7642600
Siddhanta <siddhanta.c@gmail.com>
2023-07-13 12:30:27 +0800

    added nord colorscheme

---
+++ ui/theme.go
@@ -0,0 +1,25 @@
+package ui
+
+import (
+	"puffin/ui/colorscheme"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Theme struct {
+	PrimaryBackground   lipgloss.Color
+	SecondaryBackground lipgloss.Color
+	PrimaryForeground   lipgloss.Color
+	SecondaryForeground lipgloss.Color
+	Accent              lipgloss.Color
+}
+
+func ThemeNord() Theme {
+	return Theme{
+		PrimaryBackground:   lipgloss.Color(colorscheme.Nord0),
+		SecondaryBackground: lipgloss.Color(colorscheme.Nord2),
+		PrimaryForeground:   lipgloss.Color(colorscheme.Nord6),
+		SecondaryForeground: lipgloss.Color(colorscheme.Nord4),
+		Accent:              lipgloss.Color(colorscheme.Nord10),
+	}
+}
