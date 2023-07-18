7df0b1752b02bc4fa5aeb592a8bd0ae5ede633ea
Siddhanta <siddhanta.c@gmail.com>
2023-07-18 17:44:21 +0800

    more cleanup

--- ui/balance_table.go
+++ ui/balance_table.go
@@ -7,25 +7,27 @@ import (
 
 type balanceTable struct {
 	table.Model
-	width int
+	width             int
+	columnPercentages []int
 }
 
 func newBalanceTable() *balanceTable {
-	return balanceTable{}
+	return &balanceTable{
+		columnPercentages: []int{50, 50},
+	}
 }
 
 func (b *balanceTable) SetColumns(width int) {
 }
 
 func (b *balanceTable) SetColumns2(firstRow table.Row) {
-	percentages := []int{50, 50}
-	if len(percentages) != len(firstRow) {
+	if len(b.columnPercentages) != len(firstRow) {
 		panic("length not equal")
 	}
 
 	cols := make([]table.Column, 0, len(firstRow))
 	for i, row := range firstRow {
-		c := table.Column{Title: row, Width: percent(b.width, percentages[i])}
+		c := table.Column{Title: row, Width: percent(b.width, b.columnPercentages[i])}
 		cols = append(cols, c)
 	}
 	b.Model = newDefaultTable(cols)
