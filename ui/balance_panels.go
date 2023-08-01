138e47d61211a687f374b551880b039b1b7535ee
Siddhanta <siddhanta.c@gmail.com>
2023-08-01 13:56:27 +0800

    added 2 tables side by side

--- ui/balance_panels.go
+++ ui/balance_panels.go
@@ -1,6 +1,9 @@
 package ui
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
 
 type balancePanels struct {
 	assetsTable      *TableWrapper
@@ -24,5 +27,7 @@ func (b *balancePanels) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (b *balancePanels) View() string {
-	return b.assetsTable.View()
+	return lipgloss.JoinHorizontal(lipgloss.Left,
+		b.assetsTable.View(),
+		b.assetsTable.View())
 }
