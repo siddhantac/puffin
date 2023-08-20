0f3024060e07a560c2d584e3dfe1bd0c039bd3ce
Siddhanta <siddhanta.c@gmail.com>
2023-08-20 09:45:23 +0800

    add struct member for journal file

--- hledger/hledger.go
+++ hledger/hledger.go
@@ -1,8 +1,13 @@
 package hledger
 
 type Hledger struct {
+	journalFilename string
 }
 
 func New() Hledger {
 	return Hledger{}
+}
+
+func NewWithJournalFile(filename string) Hledger {
+	return Hledger{journalFilename: filename}
 }
