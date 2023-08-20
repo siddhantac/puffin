035dcb52831e417cc752206af98011c069b63d2b
Siddhanta <siddhanta.c@gmail.com>
2023-08-20 10:16:43 +0800

    added flag for journal file

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"flag"
 	"puffin/hledger"
 	"puffin/ui"
 )
 
 func main() {
-	hl := hledger.New()
+	var journalFile string
+	flag.StringVar(&journalFile, "file", "", "journal filename")
+	flag.Parse()
+
+	var hl hledger.Hledger
+
+	if journalFile != "" {
+		hl = hledger.NewWithJournalFile(journalFile)
+	} else {
+		hl = hledger.New()
+	}
+
 	ui.New(hl).Start()
 }
