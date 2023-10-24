c4af746fdda52f1af7254602e287ec01a954912e
Siddhanta <siddhanta.c@gmail.com>
2023-10-24 17:48:50 +0800

    converted liabilities table

--- ui/balance_data.go
+++ ui/balance_data.go
@@ -21,21 +21,3 @@ func createBalanceData(data [][]string) balanceData {
 
 	return bd
 }
-
-type liabilitiesData struct {
-	Rows    []table.Row
-	Columns table.Row
-}
-
-func createLiabilitiesData(data [][]string) liabilitiesData {
-	ad := liabilitiesData{
-		Rows:    make([]table.Row, 0, len(data)),
-		Columns: data[0],
-	}
-
-	for _, d := range data[1:] {
-		ad.Rows = append(ad.Rows, d)
-	}
-
-	return ad
-}
