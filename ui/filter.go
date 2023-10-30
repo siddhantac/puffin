e4bf841b9b4a48416fe2abff63c48a89ec770636
Siddhanta <siddhanta.c@gmail.com>
2023-10-30 13:06:54 +0800

    filters working

--- ui/filter.go
+++ ui/filter.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"puffin/hledger"
+	"puffin/logger"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -23,6 +24,10 @@ func newFilter() *filter {
 	f.account = textinput.New()
 	f.account.Prompt = ""
 	f.account.Placeholder = "-"
+
+	f.date = textinput.New()
+	f.date.Prompt = ""
+	f.date.Placeholder = "-"
 	return f
 }
 
@@ -42,28 +47,58 @@ func (f *filter) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			f.Blur()
 			return f, nil
 		case "enter":
-			f.Blur()
+			// f.Blur()
 			return f, f.newFilter
 
 			// TODO: implement tab system for filters too?
 			// case key.Matches(msg, t.help.keys.Up):
 			// case key.Matches(msg, t.help.keys.Down):
+		case "down":
+			logger.Logf("filter down")
+			if f.account.Focused() {
+				f.account.Blur()
+				f.date.Focus()
+				logger.Logf("date focused")
+			}
+		case "up":
+			logger.Logf("filter up")
+			if f.date.Focused() {
+				f.date.Blur()
+				f.account.Focus()
+				logger.Logf("acc focused")
+			}
 		}
 	}
 
 	var cmd tea.Cmd
+	var cmd2 tea.Cmd
 	f.account, cmd = f.account.Update(msg)
+	f.date, cmd2 = f.date.Update(msg)
 
-	return f, cmd
+	return f, tea.Batch(cmd, cmd2)
 }
 
 func (f *filter) newFilter() tea.Msg {
-	return hledger.NewAccountFilter(f.account.Value())
+	if f.account.Focused() {
+		logger.Logf("return account filter")
+		return hledger.NewAccountFilter(
+			f.account.Value(),
+		)
+	}
+	if f.date.Focused() {
+		logger.Logf("return date filter")
+		return hledger.NewDateFilter().WithSmartDate(
+			f.date.Value(),
+		)
+	}
+
+	return nil
 }
 
 func (f *filter) Blur() {
 	f.isFocused = false
 	f.account.Blur()
+	f.date.Blur()
 }
 
 func (f *filter) Focus() {
@@ -90,13 +125,13 @@ func (f *filter) View() string {
 	accFilterData := lipgloss.NewStyle().
 		MarginBottom(1).
 		MarginRight(2).
-		Render(f.account.View()) // TODO: connect to actual filters
+		Render(f.account.View())
 
 	dateFilter := filterTitle.Render("date")
 	dateFilterData := lipgloss.NewStyle().
 		MarginBottom(1).
 		MarginRight(2).
-		Render("2023") // TODO: connect to actual filters
+		Render(f.date.View())
 
 	periodFilter := filterTitle.Render("periodic")
 	periodFilterData := lipgloss.NewStyle().
