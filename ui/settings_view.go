b818f3c9b6ef073cf621c2bfb6924008db410912
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-06-28 17:57:47 +0800

    WIP

---
+++ ui/settings_view.go
@@ -0,0 +1,64 @@
+package ui
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type settings struct {
+	keys     keyMap
+	treeView bool
+}
+
+func (s settings) Init() tea.Cmd {
+	s.treeView = true
+	return nil
+}
+
+func (s settings) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, s.keys.TreeView):
+			s.treeView = !s.treeView
+			return &s, nil
+		}
+	}
+	return &s, nil
+}
+
+func (s settings) View() string {
+	settingsTitleStyle := sectionTitleStyle.Copy().
+		MarginTop(1).
+		Render("SETTINGS")
+
+	activeTextStyle := lipgloss.NewStyle().
+		MarginRight(2)
+	inactiveTextStyle := lipgloss.NewStyle().
+		Foreground(theme.PrimaryForeground).
+		MarginRight(2)
+
+	accDepthTitle := inactiveTextStyle.Render("acct depth")
+	accDepthValue := activeTextStyle.Render(fmt.Sprintf("%d", 3))
+
+	var treeViewStyle string
+	if s.treeView {
+		treeViewStyle = activeTextStyle.MarginBottom(1).Render("tree")
+	} else {
+		treeViewStyle = inactiveTextStyle.MarginBottom(1).Render("tree")
+	}
+
+	return lipgloss.JoinVertical(
+		lipgloss.Right,
+		settingsTitleStyle,
+		treeViewStyle,
+		lipgloss.JoinVertical(
+			lipgloss.Right,
+			accDepthTitle,
+			accDepthValue,
+		),
+	)
+}
