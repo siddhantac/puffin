87376531c3ab26d9902bd29f901ebb9c61241c08
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-08-11 18:02:00 +0800

    deleted fake data

--- ui/table_graph.go
+++ ui/table_graph.go
@@ -15,27 +15,6 @@ import (
 	"github.com/siddhantac/hledger"
 )
 
-var tableData = []table.Row{
-	{"account", "commodity", "2021-12", "2022-01", "2022-02", "2022-03", "2022-04", "2022-05"},
-	{"assets:bank:chase", "$", "-800.00", "10000.00", "0", "0", "0", "-3851.65"},
-	{"assets:bank:maybank", "$", "-2300.00", "-10000.00", "0", "0", "0", "0"},
-	{"assets:bank:stanchart", "$", "0.05", "22.54", "0", "0", "0", "0"},
-	{"assets:invest:crypto", "$", "0", "0", "0", "0", "0", "2000.00"},
-	{"assets:invest:stocks", "$", "0", "0", "0", "0", "0", "1000.00"},
-	{"expenses:entertainment", "$", "0", "0", "0", "0", "0", "21.98"},
-	{"expenses:fitness", "$", "60.00", "0", "0", "0", "0", "0"},
-	{"expenses:groceries", "$", "0", "-4.88", "0", "0", "0", "8.36"},
-	{"expenses:household", "$", "800.00", "0", "0", "0", "0", "0"},
-	{"expenses:rent", "$", "2300.00", "0", "0", "0", "0", "0"},
-	{"expenses:travel", "$", "0", "0", "0", "0", "0", "15.05"},
-	{"expenses:utilities", "$", "100.54", "39.99", "0", "0", "0", "59.99"},
-	{"income:interest", "$", "0", "0", "0", "0", "0", "-22.89"},
-	{"income:others", "$", "-0.05", "0", "0", "0", "0", "0"},
-	{"liabilities:credit_card:american_express", "$", "-160.54", "-14.67", "0", "0", "0", "690.07"},
-	{"liabilities:credit_card:citibank", "$", "0", "-42.98", "0", "0", "0", "79.09"},
-	{"total", "", "0", "0", "0", "0", "0", "0"},
-}
-
 type TableGraph struct {
 	width, height int
 	table         *Table
