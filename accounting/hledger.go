d686cde285ad9f33a9505fa100f94079ba6b873f
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-08-15 12:16:08 +0800

    remove unused hledger specific stuff

--- accounting/hledger.go
+++ accounting/hledger.go
@@ -1,15 +1,3 @@
 package accounting
 
-import (
-	"github.com/siddhantac/hledger"
-)
-
-type HledgerCmd struct {
-	hldg hledger.Hledger
-}
-
-func NewHledgerCmd(hldg hledger.Hledger) HledgerCmd {
-	return HledgerCmd{hldg: hldg}
-}
-
 type MsgError string
