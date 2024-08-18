1b8ee84694c452d94f3cf96e88ceb4a287ca7a85
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-08-18 12:38:01 +0800

    fix test

--- ui/data_transformer_test.go
+++ ui/data_transformer_test.go
@@ -10,12 +10,21 @@ import (
 func TestAccountTreeMode(t *testing.T) {
 	is := is.New(t)
 
-	atm := newAccountTreeView(true, cmdBalance)
-	row := table.Row{"a:b:c", "x", "y"}
+	treeViewEnabled := func() bool { return true }
+	// treeViewDisabled := func() bool { return false }
 
-	got, err := atm.Transform(row)
+	atv := newAccountTreeView(treeViewEnabled)
+	rows := []table.Row{
+		{"a:b:c", "x", "y"},
+		{"assets:bank", "x", "y"},
+	}
+
+	err := atv.Transform(rows)
 	is.NoErr(err)
 
-	expected := "    c"
-	is.Equal(expected, got[0])
+	expected := []table.Row{
+		{"  c", "x", "y"},
+		{" bank", "x", "y"},
+	}
+	is.Equal(expected, rows)
 }
