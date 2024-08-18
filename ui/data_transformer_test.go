9b8c347794f296d5e462e0d7200b18a3a69a439f
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2024-08-18 12:39:25 +0800

    added test case

--- ui/data_transformer_test.go
+++ ui/data_transformer_test.go
@@ -10,21 +10,36 @@ import (
 func TestAccountTreeMode(t *testing.T) {
 	is := is.New(t)
 
-	treeViewEnabled := func() bool { return true }
-	// treeViewDisabled := func() bool { return false }
+	t.Run("treeViewDisabled", func(t *testing.T) {
+		treeViewEnabled := func() bool { return true }
 
-	atv := newAccountTreeView(treeViewEnabled)
-	rows := []table.Row{
-		{"a:b:c", "x", "y"},
-		{"assets:bank", "x", "y"},
-	}
+		atv := newAccountTreeView(treeViewEnabled)
+		rows := []table.Row{
+			{"a:b:c", "x", "y"},
+			{"assets:bank", "x", "y"},
+		}
 
-	err := atv.Transform(rows)
-	is.NoErr(err)
+		err := atv.Transform(rows)
+		is.NoErr(err)
 
-	expected := []table.Row{
-		{"  c", "x", "y"},
-		{" bank", "x", "y"},
-	}
-	is.Equal(expected, rows)
+		expected := []table.Row{
+			{"  c", "x", "y"},
+			{" bank", "x", "y"},
+		}
+		is.Equal(expected, rows)
+	})
+
+	t.Run("treeViewDisabled", func(t *testing.T) {
+		treeViewDisabled := func() bool { return false }
+		atv := newAccountTreeView(treeViewDisabled)
+		rows := []table.Row{
+			{"a:b:c", "x", "y"},
+			{"assets:bank", "x", "y"},
+		}
+
+		err := atv.Transform(rows)
+		is.NoErr(err)
+
+		is.Equal(rows, rows)
+	})
 }
