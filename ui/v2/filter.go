f28f3d01a51243c9868f0e2419787ce6640aeeb3
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-03-18 21:59:23 +0800

    add a way to focus on filtergroup

--- ui/v2/filter.go
+++ ui/v2/filter.go
@@ -11,24 +11,31 @@ type filter struct {
 	name string
 }
 
+var account = &filter{
+	Model: textinput.New(),
+	name:  "account",
+}
+var startDate = &filter{
+	Model: textinput.New(),
+	name:  "from",
+}
+
+var endDate = &filter{
+	Model: textinput.New(),
+	name:  "to",
+}
+
 type filterGroup struct {
-	account            *filter
-	startDate, endDate *filter
+	filters []*filter
+	focused bool
 }
 
 func newFilterGroup() *filterGroup {
 	return &filterGroup{
-		account: &filter{
-			Model: textinput.New(),
-			name:  "account",
-		},
-		startDate: &filter{
-			Model: textinput.New(),
-			name:  "from",
-		},
-		endDate: &filter{
-			Model: textinput.New(),
-			name:  "to",
+		filters: []*filter{
+			account,
+			startDate,
+			endDate,
 		},
 	}
 }
@@ -38,6 +45,13 @@ func (fg *filterGroup) Init() tea.Cmd {
 }
 
 func (fg *filterGroup) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// switch msg := msg.(type) {
+	// case tea.KeyMsg:
+	// 	switch msg.String() {
+	// 	case "tab":
+	// 		return fg, tea.Quit
+	// 	}
+	// }
 	return fg, nil
 }
 
@@ -45,17 +59,38 @@ func (fg *filterGroup) View() string {
 	filterTitleStyle := lipgloss.NewStyle().
 		Foreground(lipgloss.Color("#AAAAAA"))
 
-	accountFilter := lipgloss.JoinHorizontal(
-		lipgloss.Top,
-		filterTitleStyle.Render(fg.account.name+":"),
-		fg.account.Model.View(),
-	)
+	var view string
+	for _, f := range fg.filters {
+		filterView := lipgloss.JoinHorizontal(
+			lipgloss.Top,
+			filterTitleStyle.Render(f.name+":"),
+			f.Model.View(),
+		)
+		view = lipgloss.JoinHorizontal(lipgloss.Left, view, filterView)
+	}
+
+	borderColor := "240"
+	if fg.Focused() {
+		borderColor = "White"
+	}
 	return lipgloss.NewStyle().
 		PaddingLeft(1).
 		PaddingRight(1).
 		Border(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		Render(accountFilter)
+		BorderForeground(lipgloss.Color(borderColor)).
+		Render(view)
+}
+
+func (fg *filterGroup) Focused() bool {
+	return fg.focused
+}
+
+func (fg *filterGroup) Focus() {
+	fg.focused = true
+}
+
+func (fg *filterGroup) Blur() {
+	fg.focused = false
 }
 
 func (fg *filterGroup) ViewAlt() string {
@@ -64,8 +99,8 @@ func (fg *filterGroup) ViewAlt() string {
 
 	accountFilter := lipgloss.JoinHorizontal(
 		lipgloss.Top,
-		filterTitleStyle.Render(fg.account.name+":"),
-		fg.account.Model.View(),
+		filterTitleStyle.Render(account.name+":"),
+		account.Model.View(),
 	)
 	return lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("240")).Render(accountFilter)
 }
