4b39ec9b4ac87bbd719ef7647d0f7d53aa713a7a
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-04-15 14:13:04 +0800

    moved complex table stuff into separate file

--- ui/v2/advanced_reports.go
+++ ui/v2/advanced_reports.go
@@ -10,124 +10,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	activeTitleStyle   = lipgloss.NewStyle().Bold(true).Background(lipgloss.Color("57")).PaddingLeft(1).PaddingRight(1)
-	inactiveTitleStyle = lipgloss.NewStyle().Bold(true).PaddingLeft(1).PaddingRight(1)
-)
-
-func tblStyleActive() (table.Styles, lipgloss.Style) {
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	return s, lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("White"))
-}
-
-func tblStyleInactive() (table.Styles, lipgloss.Style) {
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("60")).
-		Bold(false)
-	return s, lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("240"))
-}
-
-type updateIncomeStatement struct{}
-
-type complexTable struct {
-	title, upperTitle, lowerTitle string
-	bottomBar                     table.Model
-	upper, lower                  table.Model
-	upperFocused                  bool
-}
-
-func newComplexTable() *complexTable {
-	return &complexTable{
-		upper:     table.New(),
-		lower:     table.New(),
-		bottomBar: table.New(),
-	}
-}
-
-func (c *complexTable) Init() tea.Cmd {
-	c.upper.Focus()
-	c.lower.Blur()
-	return nil
-}
-
-func (c *complexTable) Update(msg tea.Msg) (*complexTable, tea.Cmd) {
-	var cmd tea.Cmd
-
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "]", "[":
-			if c.upper.Focused() {
-				c.upper.Blur()
-				c.lower.Focus()
-			} else {
-				c.upper.Focus()
-				c.lower.Blur()
-			}
-		default:
-			if c.upper.Focused() {
-				c.upper, cmd = c.upper.Update(msg)
-			} else {
-				c.lower, cmd = c.lower.Update(msg)
-			}
-		}
-	}
-	return c, cmd
-}
-
-func (c *complexTable) View() string {
-	tableStyleActive, styleActive := tblStyleActive()
-	tableStyleInactive, styleInactive := tblStyleInactive()
-
-	nonInteractiveTableStyle := table.DefaultStyles()
-	nonInteractiveTableStyle.Header = nonInteractiveTableStyle.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	nonInteractiveTableStyle.Selected = nonInteractiveTableStyle.Selected.
-		Bold(false)
-	c.bottomBar.SetStyles(nonInteractiveTableStyle)
-
-	var upper, lower string
-
-	if c.upper.Focused() {
-		c.upper.SetStyles(tableStyleActive)
-		upper = styleActive.Render(c.upper.View())
-
-		c.lower.SetStyles(tableStyleInactive)
-		lower = styleInactive.Render(c.lower.View())
-	} else {
-		c.upper.SetStyles(tableStyleInactive)
-		upper = styleInactive.Render(c.upper.View())
-
-		c.lower.SetStyles(tableStyleActive)
-		lower = styleActive.Render(c.lower.View())
-	}
-	return lipgloss.JoinVertical(lipgloss.Left,
-		lipgloss.NewStyle().Bold(true).Render(c.title),
-		upper,
-		lower,
-		styleInactive.Render(c.bottomBar.View()),
-	)
-}
-
 type advancedReports struct {
 	incomeStatement2  *complexTable
 	incomeStatement   viewport.Model
@@ -243,18 +125,10 @@ func (a *advancedReports) setIncomeStatementData() {
 		DateEnd:     a.filterGroup.DateEnd(),
 		Description: a.filterGroup.Description(),
 	}
-	// data, err := a.dataProvider.IncomeStatement(filter)
-	// if err != nil {
-	// 	log.Printf("error: %v", err)
-	// 	a.incomeStatement.SetContent(err.Error())
-	// 	return
-	// }
-	// a.incomeStatement.SetContent(data)
 
 	complexTable, err := a.dataProvider.IncomeStatement2(filter)
 	if err != nil {
 		log.Printf("error: %v", err)
-		a.incomeStatement.SetContent(err.Error())
 		return
 	}
 	a.incomeStatement2.title = complexTable.Title
