35733f11f54f5a37a48a419e54a65df1b7febdb7
Siddhanta <66236925+siddhantac@users.noreply.github.com>
2025-04-20 22:56:09 +0800

    moved styles into another file

--- ui/v2/complex_table.go
+++ ui/v2/complex_table.go
@@ -6,39 +6,6 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	activeTitleStyle   = lipgloss.NewStyle().Bold(true).Background(lipgloss.Color("57")).PaddingLeft(1).PaddingRight(1)
-	inactiveTitleStyle = lipgloss.NewStyle().Bold(true).PaddingLeft(1).PaddingRight(1)
-)
-
-func tblStyleActive() (table.Styles, lipgloss.Style) {
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("57")).
-		Bold(false)
-	return s, lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("White"))
-}
-
-func tblStyleInactive() (table.Styles, lipgloss.Style) {
-	s := table.DefaultStyles()
-	s.Header = s.Header.
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("240")).
-		BorderBottom(true).
-		Bold(false)
-	s.Selected = s.Selected.
-		Foreground(lipgloss.Color("229")).
-		Background(lipgloss.Color("60")).
-		Bold(false)
-	return s, lipgloss.NewStyle().Border(lipgloss.RoundedBorder()).BorderForeground(lipgloss.Color("240"))
-}
-
 type complexTable struct {
 	title, upperTitle, lowerTitle string
 	bottomBar                     table.Model
